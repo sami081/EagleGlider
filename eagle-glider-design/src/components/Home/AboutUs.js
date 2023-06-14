ba9672668175dde156c47a5015b9e455ffa1dafb
sami081 <makhloufisami0811@gmail.com>
2023-06-14 02:28:37 +0200

    about-us

--- eagle-glider-design/src/components/Home/AboutUs.js
+++ eagle-glider-design/src/components/Home/AboutUs.js
@@ -9,19 +9,41 @@ const AboutUs = () => {
           <img src="./ressources/images/picture/ogunta.png" alt="ogunta" />
           <div className="information-character information-ogunta">
             <div className="inside-box inside-box-ogunta">
-
-            <h3>Ogunta</h3>
-            <p>
-              Il est passionné par le dessin et le design. De formation
-              webdesigner, il a le désir de s'améliorer constamment pour être le
-              plus compétent possible. D'une personnalité calme et posée, il
-              accepte volontiers les différentes épreuves qui s'annoncent devant
-              lui.
-            </p>
+              <h3>Ogunta</h3>
+              <p>
+                Il est passionné par le dessin et le design. De formation
+                webdesigner, il a le désir de s'améliorer constamment pour être
+                le plus compétent possible. D'une personnalité calme et posée,
+                il accepte volontiers les différentes épreuves qui s'annoncent
+                devant lui.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="character sami">
+          <img src="./ressources/images/picture/ogunta.png" alt="ogunta" />
+          <div className="information-character information-sami">
+            <div className="inside-box inside-box-sami">
+              <h3>Sami</h3>
+              <p>
+                Poussée par de grandes ambitions, Sami poursuit son objectif de
+                créer sa propre entreprise. La manière d’y parvenir est tout
+                aussi importante pour lui. Pour y arriver, il s’est formé au
+                développement web. Ses compétences en leadership lui donnent la
+                structure dont il a besoin pour être pragmatique et efficace.
+              </p>
             </div>
           </div>
         </div>
       </div>
+      <aside>
+        <p>
+          A la fin d’une conversation, ils ont réalisé la valeur ajoutée qu’ils
+          pouvaient s’apporter l'un à l'autre. A partir de ce moment, la
+          décision de créer un projet commun est née. Malgré leurs histoires
+          différentes, ils unissent leurs passions respectives.
+        </p>
+      </aside>
     </section>
   );
 };
